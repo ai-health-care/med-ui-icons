cc94e46f09f76f2a0cfe3ce18027e12b8f3df486
Henry Krumb <mail@henrykrumb.de>
2021-06-24 00:05:14 +0200

    replace pastel colors with darker colors in dark theme

--- scripts/dark-from-white.sh
+++ scripts/dark-from-white.sh
@@ -9,5 +9,12 @@ mkdir -p ../svg-dark
 cp ../svg-white/*.svg ../svg-dark
 
 for svg in ../svg-dark/*.svg; do
-    sed -i 's/fill:#ffffff/#202020/g' "$svg"
+    # replace white --> black
+    sed -i 's/fill:#ffffff/fill:#202020/g' "$svg"
+    # replace green
+    sed -i 's/#87deaa/#008033/g' "$svg"
+    # replace blue
+    sed -i 's/#87aade/#3771c8/g' "$svg"
+    # replace yellow
+    sed -i 's/#ffcc00/#aa8800/g' "$svg"
 done

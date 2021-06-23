c829b025a2fed05155fa65f89cde1917740f959a
Henry Krumb <mail@henrykrumb.de>
2021-06-23 14:49:50 +0200

    add png generator script

---
+++ scripts/generate-png.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# generate dark icon set by replacing white --> black
+
+scriptdir=$(dirname "$0")
+cd "$scriptdir"
+
+./dark-from-white
+
+mkdir -p ../png-white
+mkdir -p ../png-dark
+
+for svg in ../svg-white/*.svg; do
+    png=../png-white/$(basename "$svg")
+    inkscape "$svg" --export-type=png --export-filename="$png"
+done
+
+for svg in ../svg-dark/*.svg; do
+    png=../png-dark/$(basename "$svg")
+    inkscape "$svg" --export-type=png --export-filename="$png"
+done
